a2d8e00decf2da64c2f208e636b44e98cdd89bb4
shruthi43 <shruthiparvatham2000@gmail.com>
2020-05-17 14:35:30 +0530

    new sample

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace devops_sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
